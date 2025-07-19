af7b109f1bec87ceffb3159ed9129ba308f8610f
xanaxhol1c <loram.hoptan@gmail.com>
2025-07-19 19:04:07 +0300

    added recommendations for video details page

--- video_player_site/videos/views.py
+++ video_player_site/videos/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Video
 from .utils import open_file
 from django.http import StreamingHttpResponse
+from django.db.models import Case, When, Value, IntegerField
 
 # Create your views here.
 def video_details(request, slug=None):
-    video = Video.objects.filter(slug=slug).first()
-    return render(request, 'videos/video_details.html', {'video' : video})
+    video = get_object_or_404(Video, slug=slug)
+
+    category = video.category
+
+    recommendations = Video.objects.annotate(
+        priority=Case(
+            When(category=category, then=Value(0)),
+            default=Value(1),
+            output_field=IntegerField()
+        )
+    ).order_by('priority').exclude(name=video.name)[:4]
+    return render(request, 'videos/video_details.html', {'video' : video, 'recommendations' : recommendations})
 
 # def stream_video(request, video_id):
 #     file, status_code, content_length, content_range = open_file(request, video_id)
