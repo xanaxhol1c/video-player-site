5ae86f2d1bca015162a83ab868fdd517035b93bb
xanaxhol1c <loram.hoptan@gmail.com>
2025-07-20 16:05:43 +0300

    added pagination

--- video_player_site/main/views.py
+++ video_player_site/main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from videos.models import Video
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
+    page = request.GET.get('page', 1)
     videos = Video.objects.all()
-    return render(request, 'main/index.html', {'videos' : videos})
+    paginator = Paginator(videos, 3)
+    
+    current_page = paginator.page((int(page)))
+    return render(request, 'main/index.html', {'videos' : current_page})
