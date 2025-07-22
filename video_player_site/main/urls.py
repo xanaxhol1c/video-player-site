a083b4f2dc547b7bad000fd2e7eac2b0e3f749ec
xanaxhol1c <loram.hoptan@gmail.com>
2025-07-22 23:56:08 +0300

    added upload form and view for creators

--- video_player_site/main/urls.py
+++ video_player_site/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, for_creators
 
 app_name="main"
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('for-creators/', for_creators, name='for_creators')
 ]
--- video_player_site/main/views.py
+++ video_player_site/main/views.py
@@ -1,6 +1,10 @@
+from .forms import UploadVideoForm
 from django.shortcuts import render
-from videos.models import Video
+from videos.models import Video, Category
 from django.core.paginator import Paginator
+from django.http.response import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
@@ -10,3 +14,28 @@ def index(request):
     
     current_page = paginator.page((int(page)))
     return render(request, 'main/index.html', {'videos' : current_page})
+
+@login_required
+def for_creators(request):
+    user = request.user
+
+    try:
+        if user.role.name != "Creator":
+            return HttpResponseForbidden("Access denied")
+    except AttributeError:
+        return HttpResponseForbidden("Access denied")
+    
+    if request.method == 'POST':
+        form = UploadVideoForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form.save()
+            print('form saved successfully')
+            #return redirect('main:upload_video_completed')
+
+    else:
+        form = UploadVideoForm()
+
+    categories = Category.objects.all()
+
+    return render(request, 'main/for_creators.html', {'categories' : categories, 'form' : form})
