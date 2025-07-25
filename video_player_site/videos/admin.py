b0a170512d0720b67f0fa06f566e12ecd499ccf6
xanaxhol1c <loram.hoptan@gmail.com>
2025-07-25 12:41:55 +0300

    added actions to approve/hide videos

--- video_player_site/videos/admin.py
+++ video_player_site/videos/admin.py
@@ -13,3 +13,20 @@ class VideoAdmin(admin.ModelAdmin):
     list_filter = ['name', 'category', 'is_published', 'created_at']
     prepopulated_fields = {'slug' : ('name',)}
     readonly_fields = ['views']
+
+    actions = ['approve_videos', 'hide_videos']
+
+    def approve_videos(self, request, queryset):
+        for video in queryset:
+            if not video.is_published:
+                video.is_published = True
+                video.save()
+
+    def hide_videos(self, request, queryset):
+        for video in queryset:
+            if video.is_published:
+                video.is_published = False
+                video.save()
+
+    approve_videos.short_description = '✅ Approve selected videos'
+    hide_videos.short_description = '❌ Hide selected videos'
