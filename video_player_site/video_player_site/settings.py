cbaf1eb1d6b2016efcf4561a641f4a0dcaede1e5
xanaxhol1c <loram.hoptan@gmail.com>
2025-07-28 19:45:21 +0300

    changed db

--- video_player_site/video_player_site/settings.py
+++ video_player_site/video_player_site/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 import cloudinary
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,10 +108,8 @@ WSGI_APPLICATION = 'video_player_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DB_EXTERNAL'),
+                                     conn_max_age=600)
 }
 
 CACHES = {
